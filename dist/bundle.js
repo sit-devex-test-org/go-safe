8bcaf0e2725da7b402482a108266090d5527d574
xopham <xopham@users.noreply.github.com>
2024-11-28 17:54:43 +0000

    deploy: 75fe18f68dac97fc82496f005f29fd8de20fbc1d

--- dist/bundle.js
+++ dist/bundle.js
@@ -963,7 +963,7 @@ const output = cowsay.say({
   text: getRandomQuote()
 });
 function getRandomQuote() {
-  const quotes = ["Security is not a product, but a process.", "The problem is not the user’s lack of knowledge, but the system’s lack of guidance.", "The only secure system is one that is unplugged, turned off, and locked in a room.", "If you think technology can solve your security problems, then you don’t understand the problems and you don’t understand the technology.", "Data is a precious thing and will last longer than the systems themselves."];
+  const quotes = ["Security is not a product, but a process.", "The problem is not the user’s lack of knowledge, but the system’s lack of guidance.", "The only secure system is one that is unplugged, turned off, and locked in a room.", "If you think technology can solve your security problems, then you don’t understand the problems and you don’t understand the technology.", "Data is a precious thing and will last longer than the systems themselves.", "Passwords are like toothbrushes: choose a good one, don’t share it, and change it regularly."];
 
   // Generate a cryptographically secure random index
   const randomIndex = crypto.getRandomValues(new Uint32Array(1))[0] % quotes.length;
